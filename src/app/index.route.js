10f0996af13c1c08dcc72f0fc23e12fef54bedee
simonasdev <simonasgildutis@gmail.com>
2015-09-26 13:59:28 +0300

    Routes

--- src/app/index.route.js
+++ src/app/index.route.js
@@ -3,19 +3,44 @@
 
   angular
     .module('game')
-    .config(routeConfig);
+    .config(routeConfig)
+    .run(runFn);
 
   /** @ngInject */
   function routeConfig($stateProvider, $urlRouterProvider) {
     $stateProvider
-      .state('home', {
+      .state('root', {
         url: '/',
+        absctract: true,
         templateUrl: 'app/main/main.html',
         controller: 'MainController',
-        controllerAs: 'main'
-      });
+        controllerAs: 'main',
+        resolve: {
+          player: ensurePlayer
+        }
+      })
+      .state('root.game', {
+        url: '',
+        templateUrl: 'app/components/map.html',
+        controller: 'MapController'
+      })
+      .state('player', {
+        url: '/git-gud',
+        templateUrl: 'app/components/player.html',
+        controller: 'PlayerController'
+      })
 
     $urlRouterProvider.otherwise('/');
+  }
+
+  function ensurePlayer ($http) {
+    
+  }
+
+  function runFn ($rootScope, $state) {
+    $rootScope.$on('$stateChangeError', function (_event, _toState, _toParams, _fromState, _fromParams, _error) {
+      $state.go('player');
+    });
   }
 
 })();

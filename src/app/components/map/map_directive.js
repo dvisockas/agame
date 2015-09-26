dd2a5da38772ed2186b83d2443418cd0c3bae5fe
simonasdev <simonasgildutis@gmail.com>
2015-09-27 02:20:32 +0300

    Bounds: g

--- src/app/components/map/map_directive.js
+++ src/app/components/map/map_directive.js
@@ -127,11 +127,11 @@ angular.module('game')
           if (playerMarker) {
             playerMarker.setLatLng(coords);
           } else {
-            // playerMarker = L.marker(coords, {
-            //   icon: playerIcon,
-            //   zIndexOffset: 1000,
-            //   player: $scope.player
-            // }).addTo(map);
+            playerMarker = L.marker(coords, {
+              icon: playerIcon,
+              zIndexOffset: 1000,
+              player: $scope.player
+            }).addTo(map);
 
             map.setView(coords, 18, {
               animate: true
@@ -168,8 +168,8 @@ angular.module('game')
 
         function getMaxBounds () {
           var player = $scope.player,
-              lat = 0.5 * (1 / 110.574),
-              lon = 0.5 * (1 / (111.32 * Math.cos(player.latitude)));
+              lat = 0.6 * (1 / 110.574),
+              lon = 0.6 * (1 / (111.32 * Math.cos(player.latitude)));
 
           if (player) {
             return [
